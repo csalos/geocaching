2c1607c3c7a85f6762ee12d78f564f7def0477a7
Csa.Csi87 <csalos.csilla@gmail.com>
2026-04-13 12:52:59 +0200

    -maradt
    nem kell beírni, ha maradt

--- geo.js
+++ geo.js
@@ -1,6 +1,6 @@
 let arrMozgo = [ 
     [ 3825,   "FV", "Forrásvadász", [ ["2026.02.24", "Tókaji forrás", "Húsvét forrás"] ] ], 
-    [ 1960, "KaKu", "Kálváriakutató", [ ["2026.01.27", "Kaposvár", "Maradt"] ] ], 
+    [ 1960, "KaKu", "Kálváriakutató", [ ["2026.01.27", "Kaposvár"] ] ], 
     [  212, "M001", "mobil_001", [ ["2025.12.23", "Fonyód", "#GeoKari"] ] ], 
     [ 1248, "M002", "Kedvelt helyeink", [
          ["2025.12.06", "Orfű", "#Walk41"],
@@ -10,16 +10,16 @@ let arrMozgo = [
          ["2025.12.06", "Orfű", "#Walk41"], 
          ["2025.12.31", "Somogyvár", "#GeoSzilveszter"],
          ["2026.02.15", "Ságvár", "#Walk43"] ] ],
-    [ 4482,  "KAS", "Kastélykutató", [ ["2024.07.18", "Várda", "Maradt"] ] ], 
+    [ 4482,  "KAS", "Kastélykutató", [ ["2024.07.18", "Várda"] ] ], 
     [ 6235, "TJEL", "Turistajelzések", [ ["2024.04.06", "Tókaji parkerdő", "Töröcskei tó"] ] ], 
     [ 5543, "EHAZ", "Erdei házak, kunyhók", [
-         ["2023.08.12", "Várda", "Maradt"], 
+         ["2023.08.12", "Várda"], 
          ["2026.02.15", "Ságvár", "#Walk43"] ] ], 
     [ 5081, "Time", "Napóra", [
          ["2023.05.20", "Kaposvár", "Hencse"], 
          ["2023.10.29", "Kaposvár"], 
          ["2025.12.06", "Orfű", "#Walk41"] ] ], 
-    [ 1779,  "AKT", "Árpád-kori templomromok, templomok 4.0", [ ["2021.12.03", "Kaposszentjakab", "Maradt"] ] ],  
+    [ 1779,  "AKT", "Árpád-kori templomromok, templomok 4.0", [ ["2021.12.03", "Kaposszentjakab"] ] ],  
     [ 1458,   "PC", "Gécépécé (a HiTech mozgó)", [ ["2019.12.30", "Kaposvár", "Tura"] ] ], 
     [ 1784,   "TO", "Tóbarát", [ ["2019.12.30", "Kaposvár", "Tura"] ] ], 
     [ 4773, "HIRE", "Híres emberek", [ ["2019.08.17", "Kaposvár", "Kaposvár"] ] ], 
@@ -27,17 +27,17 @@ let arrMozgo = [
          ["2014.08.24", "Kaposvár", "Haza"], 
          ["2021.04.27", "Kaposvár", "Hetes"] ] ], 
     [ 3959, "FENT", "Fentről a világ (fotózz panorámát)", [
-         ["2014.07.03", "Kaposvár", "Maradt"], 
+         ["2014.07.03", "Kaposvár"], 
          ["2026.02.15", "Ságvár", "#Walk43"] ] ], 
     [ 3889, "MKAT", "Magyarország katonái", [
-         ["2014.05.27", "Kaposvár", "Maradt"], 
-         ["2023.08.01", "Gölle", "Maradt"] ] ], 
+         ["2014.05.27", "Kaposvár"], 
+         ["2023.08.01", "Gölle"] ] ], 
     [  968, "Izgi", "Izgimozgi", [
          ["2014.04.20", "Kaposvár", "Kaposvár"], 
          ["2025.12.06", "Orfű", "#Walk41"] ] ], 
-    [ 3806,  "KEM", "Kerékpáros mozgó", [ ["2014.04.19", "Kaposvár", "Maradt"] ] ], 
+    [ 3806,  "KEM", "Kerékpáros mozgó", [ ["2014.04.19", "Kaposvár"] ] ], 
     [ 4856, "DDK2", "Rockenbauer Pál Dél-dunántúli Kéktúra", [ ["2014.04.10", "Szenna", "Szenna tető"] ] ], 
-    [ 4456,  "MLM", "A malomnak nincsen köve", [ [ "2014.01.02", "Kaposvár", "Maradt"] ] ], 
+    [ 4456,  "MLM", "A malomnak nincsen köve", [ [ "2014.01.02", "Kaposvár"] ] ], 
     [ 1409, "SPIR", "Szellemjáró Geoláda", [
          ["2013.08.21", "Taszár", "Somogysárd"],
          ["2026.02.15", "Ságvár", "#Walk43"] ] ] 
