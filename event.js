b213c3a61852dd879dd10ed710398f9de40a5162
Csa.Csi87 <csalos.csilla@gmail.com>
2026-08-06 18:09:04 +0200

    try an other array struct

--- event.js
+++ event.js
@@ -3,54 +3,51 @@ document.write(tableEvent);
 
 let arrEvent = [ 
     [ 6767, "50FV", "L. Geocaching Fesztivál és Verseny", "2026.05.16", "Sasrét"], 
-    [ 0, 
-	 	[[6709,"25ZS"], 
-		 [6712,"25TL"], 
-		 [6722,"25UV"],
-		 [6725,"25SK"],
-		 [6744,"25BS"],
-		 [6723,"25BM"],
-		 [6737,"25HB"],
-		 [6724,"25BU"],
-		 [6699,"25MF"]], "Jubileumi körtúra", 
-	 	"2026.05.01\n2026.06.13\n2026.06.13\n2026.07.29\n2026.07.29\n2026.07.30\n2026.07.31\n2026.07.31\n2026.08.01", 
-	 	"Zselic + Kapos völgye\nSzekszárdi dombság\nÜvegesek útja - Mecsek\nA szegedi kisvasút nyomában\nBékési ősök nyomában\nMályvádi huszonötös\nDebrecen - Nagyerdei körtúra\nBükki körtúra\nMedves-fennsík"],
-    [ 6755, "GN26", "Geo Nyuszi '26", "2026.04.03", "Kaposvár - Deseda"], 
-    [ 0, 
-		[[6730,"MO02"],
-		 [6710,"MO15"],
-		 [6733,"MO17"],
-		 [6719,"MO01"],
-		 [6692,"MO10"],
-		 [6746,"MO11"],
-		 [6689,"MO23"],
-		 [6698,"MO06"],
-		 [6711,"MO03"],
-		 [6707,"MO22"],
-		 [6726,"MO09"],
-		 [6702,"MO16"],
-		 [6729,"MO04"],
-		 [6708,"MO13"]], "Megyei Óriások",
-	 	"2026.03.21\n2026.03.21\n2026.04.10\n2026.04.10\n2026.04.10\n2026.04.10\n2026.06.13\n2026.07.29\n2026.07.29\n2026.07.30\n2026.07.30\n2026.07.31\n2026.07.31\n2026.08.01",
-		"Sasréti ősbükkös\nGyöngyöspusztai védett tölgyfasor\nKasztói őstölgyes\nIzsáki Csodafa\nKiskörei \"300\" éves tölgy\nA Nagykunság legidősebb tölgyfája\nHorvátország egyetlene\nA Körtvélyesi matuzsálem\nViharsarki matuzsálem\nA remetei korelnök\nBagaméri óriások\nBalkányi felhőkarcoló\nMagyarország legmagasabb fája\nA keselyréti 500 éves tölgyfa"], 
-    [ 6695, "2025", "Geoszilveszter 2025", "2025.12.31", "Somogyvár"], 
-    [ 6694, "XM25", "Geokarácsony 2025", "2025.12.23", "Fonyód"], 
-    [ 6682, "GM25", "Geo Miki '25", "2025.12.06", "Dombóvár"], 
-    [ 6604, "GN25", "Geo Nyuszi '25", "2025.04.20", "Kaposvár"], 
-    [ 6562, "GM24", "Geo Miki '24", "2024.12.07", "Kaposvár"], 
-    [ 6141, "KozM", "Közlekedési Múzeum az Északiban", "2024.10.11", "Budapest"],  
-    [ 6334, "XM22", "Geokarácsony 2022", "2023.01.02", "Kaposvár"], 
-    [ 5998, "MVK1", "Magyar Vöröskereszt - Jean-Henri Dunant szobra", "2021.10.12", "Budapest"], 
-    [ 0, 
-	 	[[6047,"XX17"],
-		 [6031,"XX01"],
-		 [6044,"XX14"],
-		 [6035,"XX05"],
-		 [5239,"20ZS"],
-		 [6045,"XX15"],
-		 [6036,"XX06"]], "20 éves a geocaching.hu",
-	 	"2021.07.23\n2021.08.16\n2021.10.09\n2025.09.30\n2025.10.23\n2026.07.05\n2026.07.29", 
-	 	"Gunarasi parkerdő\nÉrsekcsanád\nÉrd - Papi földek\nSashalmi-erdő\nZselic\nSzent Vendel kilátó\nKivéve a gyevi bírót"], 
+    [ 0, "Jubileumi körtúra", 
+	 	[[6709,"25ZS","2026.05.01","Zselic + Kapos völgye"],
+		 [6712,"25TL","2026.06.13","Szekszárdi dombság"],
+		 [6722,"25UV","2026.06.13","Üvegesek útja - Mecsek"],
+		 [6725,"25SK","2026.07.29","A szegedi kisvasút nyomában"],
+		 [6744,"25BS","2026.07.29","Békési ősök nyomában"],
+		 [6723,"25BM","2026.07.30","Mályvádi huszonötös"],
+		 [6737,"25HB","2026.07.31","Debrecen - Nagyerdei körtúra"],
+		 [6724,"25BU","2026.07.31","Bükki körtúra"],
+		 [6699,"25MF","2026.08.01","Medves-fennsík"]]
+	],
+    [ 6755, "GN26", "Geo Nyuszi '26", "2026.04.03", "Kaposvár - Deseda"],
+    [ 0, "Megyei Óriások",
+		[[6730,"MO02","2026.03.21","Sasréti ősbükkös"],
+		 [6710,"MO15","2026.03.21","Gyöngyöspusztai védett tölgyfasor"],
+		 [6733,"MO17","2026.04.10","Kasztói őstölgyes"],
+		 [6719,"MO01","2026.04.10","Izsáki Csodafa"],
+		 [6692,"MO10","2026.04.10","Kiskörei \"300\" éves tölgy"],
+		 [6746,"MO11","2026.04.10","A Nagykunság legidősebb tölgyfája"],
+		 [6689,"MO23","2026.06.13","Horvátország egyetlene"],
+		 [6698,"MO06","2026.07.29","A Körtvélyesi matuzsálem"],
+		 [6711,"MO03","2026.07.29","Viharsarki matuzsálem"],
+		 [6707,"MO22","2026.07.30","A remetei korelnök"],
+		 [6726,"MO09","2026.07.30","Bagaméri óriások"],
+		 [6702,"MO16","2026.07.31","Balkányi \"felhőkarcoló\""],
+		 [6729,"MO04","2026.07.31","Magyarország legmagasabb fája"],
+		 [6708,"MO13","2026.08.01","A keselyréti 500 éves tölgyfa"]]
+	],
+    [ 6695, "2025", "Geoszilveszter 2025", "2025.12.31", "Somogyvár"],
+    [ 6694, "XM25", "Geokarácsony 2025", "2025.12.23", "Fonyód"],
+    [ 6682, "GM25", "Geo Miki '25", "2025.12.06", "Dombóvár"],
+    [ 6604, "GN25", "Geo Nyuszi '25", "2025.04.20", "Kaposvár"],
+    [ 6562, "GM24", "Geo Miki '24", "2024.12.07", "Kaposvár"],
+    [ 6141, "KozM", "Közlekedési Múzeum az Északiban", "2024.10.11", "Budapest"],
+    [ 6334, "XM22", "Geokarácsony 2022", "2023.01.02", "Kaposvár"],
+    [ 5998, "MVK1", "Magyar Vöröskereszt - Jean-Henri Dunant szobra", "2021.10.12", "Budapest"],
+    [ 0, "20 éves a geocaching.hu",
+	 	[[6047,"XX17","2021.07.23","Gunarasi parkerdő"],
+		 [6031,"XX01","2021.08.16","Érsekcsanád"],
+		 [6044,"XX14","2021.10.09","Érd - Papi földek"],
+		 [6035,"XX05","2025.09.30","Sashalmi-erdő"],
+		 [5239,"20ZS","2025.10.23","Zselic"],
+		 [6045,"XX15","2026.07.05","Szent Vendel kilátó"],
+		 [6036,"XX06","2026.07.29","Kivéve a gyevi bírót"]]
+	], 
     [ 5775, 2019, "Geoszilveszter 2019", "2019.12.30", "Kaposvár"]
 ];
 
@@ -58,31 +55,43 @@ let arrEvent = [
 //------------------------------------------------
 arrEvent.forEach((element) => addEvent(element));
 
-function addEvent(arr) { 
-    let tr = document.createElement("tr"); 
-    tr.style = ""; 
-    let td = document.createElement("td"); 
-    if(arr[0]!==0) { 
-        let a = document.createElement('a'); 
-        a.href = 'https://geocaching.hu/caches.geo?id='+arr[0]; 
-        a.appendChild(document.createTextNode("GC"+arr[1])); 
-        td.appendChild(a); 
-    } else { 
-        let link = arr[1]; 
-        for(let x=0; x<link.length; x++) { 
-            let b = document.createElement('a'); 
-            b.href = 'https://geocaching.hu/caches.geo?id='+link[x][0]; 
-            b.appendChild(document.createTextNode("GC"+link[x][1])); 
-            td.appendChild(b); 
-            td.appendChild(document.createElement('br')); 
-        } 
-    } 
-    tr.appendChild(td); 
-    for(let i=2; i<5; i++) { 
-        let td = document.createElement("td"); 
-        let tx = document.createTextNode(arr[i]); 
-        td.appendChild(tx); 
-        tr.appendChild(td); 
+function addEvent(arr) {
+    let tr = document.createElement("tr");
+    tr.style = "";
+    if(arr[0]!==0) {
+		let td = document.createElement("td");
+        let a = document.createElement('a');
+        a.href = 'https://geocaching.hu/caches.geo?id='+arr[0];
+        a.appendChild(document.createTextNode("GC"+arr[1]));
+        td.appendChild(a);
+		tr.appendChild(td);
+	    for(let i=2; i<5; i++) {
+	        let td = document.createElement("td");
+	        let tx = document.createTextNode(arr[i]);
+	        td.appendChild(tx);
+	        tr.appendChild(td);
+	    }
+    } else {
+        let link = arr[2];
+		let td_id = document.createElement("td");
+		let td = document.createElement("td");
+		let td_dt = document.createElement("td");
+		let td_nm = document.createElement("td");
+        for(let x=0; x<link.length; x++) {
+            let b = document.createElement('a');
+            b.href = 'https://geocaching.hu/caches.geo?id='+link[x][0];
+            b.appendChild(document.createTextNode("GC"+link[x][1]));
+            td_id.appendChild(b);
+            td_id.appendChild(document.createElement('br'));
+            td_dt.appendChild(document.createTextNode(link[x][2]));
+            td_dt.appendChild(document.createElement('br'));
+            td_nm.appendChild(document.createTextNode(link[x][3]));
+            td_nm.appendChild(document.createElement('br'));
+        }
+		tr.appendChild(td_id); //id és link
+		tr.appendChild(td.appendChild(document.createTextNode(arr[1]))); //csoport név
+		tr.appendChild(td_dt); //megtalálás napja
+		tr.appendChild(td_nm); //láda neve
     }
     document.getElementById("rowsEvent").after(tr);
 }
