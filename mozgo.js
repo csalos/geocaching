ea68d33636e7b91e959e33c8f7bf5376effaaa95
Csa.Csi87 <csalos.csilla@gmail.com>
2026-08-10 18:58:55 +0200

    Update mozgo.js

--- mozgo.js
+++ mozgo.js
@@ -12,15 +12,17 @@ async function getMozgoList() {
 		if (!response1.ok) throw new Error("API 1.hívás sikertelen");
         const jsn1 = await response1.json();
 
-		//megtalálások lekérése: dátum és bejegyzés
+		//megtalálások lekérése: láda azonosító, dátum, bejegyzés és a log típusa
+		//egybe - hogy ne terheljük le a szervert a sok hívással
         const response2 = await fetch("https://api.geocaching.hu/logsbyuser?fields=cache_id%2Cdate%2Cnotes%2Clogtype&dir=asc&userid="+myUserId);
 		if (!response2.ok) throw new Error("API 2.hívás sikertelen");
         const jsn2 = await response2.json();
 
 		for(const láda of jsn1) {
-			// Szűrés
+			// megtalálásokból leszűrjük az adott mozgóhoz tartozókat, ha a bejegyzés típusa "1" - azaz "megtalált" 
 			const talalatok = jsn2.filter(elem => elem.cache_id === láda.id && elem.logtype==="1");
-            await jsonMozgoMolyolo(láda, talalatok);
+			//majd ez végig molyoljuk
+            jsonMozgoMolyolo(láda, talalatok);
         }
 		//táblázat kiszínezése
 		const rows = document.querySelectorAll('#mozgo tr:nth-child(odd), #event tr:nth-child(odd)');
@@ -33,48 +35,48 @@ async function getMozgoList() {
 	}
 }
 
-//megtalált mozgók listájának átmolyolása
+/*megtalált mozgók listájának átmolyolása
+* @param láda [object] - adott mozgóhoz tartozó adatok (név, azonosító)
+* @param logs [array of object] - adott ládára leszűrt log bejegyzések
+*/
 var finds = [];
-async function jsonMozgoMolyolo(láda, logs) {
-    try {
-		//új sor a táblázatban
-		let tr = document.createElement("tr"); 
-	    //láda azonosító és link
-	    let tdLink = document.createElement("td"); 
-	    let a = document.createElement('a'); 
-	    a.href = 'https://geocaching.hu/caches.geo?id='+láda.id; 
-	    a.appendChild(document.createTextNode(láda.waypoint)); 
-	    tdLink.appendChild(a); 
-	    tr.appendChild(tdLink);
+function jsonMozgoMolyolo(láda, logs) {
+	//új sor a táblázatban
+	let tr = document.createElement("tr"); 
+	//láda azonosító és link
+	let tdLink = document.createElement("td"); 
+	let a = document.createElement('a'); 
+	a.href = 'https://geocaching.hu/caches.geo?id='+láda.id; 
+	a.appendChild(document.createTextNode(láda.waypoint)); 
+	tdLink.appendChild(a); 
+	tr.appendChild(tdLink);
 	
-	    //láda neve
-	    let tdName = document.createElement("td"); 
-	    let txName = document.createTextNode(láda.nickname); 
-	    tdName.appendChild(txName); 
-	    tr.appendChild(tdName);
+	//láda neve
+	let tdName = document.createElement("td"); 
+	let txName = document.createTextNode(láda.nickname); 
+	tdName.appendChild(txName); 
+	tr.appendChild(tdName);
 
-	    finds = [];
-        logs.forEach(kiíró);
+	let finds = logs.flatMap(kiíró);
 	    
-		//mikor, honnan, hová
-	    let f = ["", "", ""];
-	    for(let i=0; i<finds.length; i++) { 
-	        f[0] = f[0].concat(finds[i][0], (finds.length>1)?"\n":"");
-	        f[1] = f[1].concat(finds[i][1], (finds.length>1)?"\n":"");
-	        f[2] = f[2].concat(finds[i][2]??"Maradt", (finds.length>1)?"\n":"");
-	    }
-	    for(let j=0; j<3; j++) { 
-	        let td = document.createElement("td"); 
-	        let tx = document.createTextNode(f[j]); 
-	        td.appendChild(tx); 
-	        tr.appendChild(td); 
-	    } 
-	    document.getElementById("rowsMozgo").before(tr); 
-    } catch (hiba) {
-        console.error("Hiba a lekérésnél:", hiba);
+	//mikor, honnan, hová
+	let f = ["", "", ""];
+	for(let i=0; i<finds.length; i++) { 
+		f[0] = f[0].concat(finds[i][0], (finds.length>1)?"\n":"");
+		f[1] = f[1].concat(finds[i][1], (finds.length>1)?"\n":"");
+		f[2] = f[2].concat(finds[i][2]??"Maradt", (finds.length>1)?"\n":"");
 	}
+	for(let j=0; j<3; j++) { 
+		let td = document.createElement("td"); 
+		let tx = document.createTextNode(f[j]); 
+		td.appendChild(tx); 
+		tr.appendChild(td); 
+	} 
+	document.getElementById("rowsMozgo").before(tr); 
 }
-//bejegyzésből kiszedni honnan/hová ment a láda
+/*bejegyzésből kiszedni honnan/hová ment a láda
+* @param log [object] - adott ládára leszűrt log bejegyzés
+*/
 function kiíró(log) {
 	text = log.notes;	
     // A regex: megkeresi a ___ közötti részeket
@@ -83,17 +85,31 @@ function kiíró(log) {
     // Az összes találat kinyerése
     const matches = [...text.matchAll(regex)];
 
-    matches.forEach(match => {
+    /*matches.forEach(match => {
       const content = match[1];
       const parts = content.split('_');
 	  const dátum = log.date.split(' ')[0].replace(/-/g, '.'); // dátum formátum: yyyy.mm.dd
 
       if (parts.length === 2) {
         //console.log("Típus: Útvonal | Honnan: "+parts[0]+", Hova: "+parts[1]);
-		finds.push( [ dátum, parts[0], parts[1] ] );
+		return [ dátum, parts[0], parts[1] ];
       } else {
         //console.log("Típus: Helyszín | Hol: "+parts[0]);
 		finds.push( [ dátum, parts[0] ] );
       }
-    });
+    });*/
+
+	return matches.map(match => {
+		const content = match[1];
+		const parts = content.split('_');
+		const dátum = log.date.split(' ')[0].replace(/-/g, '.'); // dátum formátum: yyyy.mm.dd
+
+		if (parts.length === 2) {
+			// console.log("Típus: Útvonal | Honnan: "+parts[0]+", Hova: "+parts[1]);
+			return [dátum, parts[0], parts[1]];
+		} else {
+			//console.log("Típus: Helyszín | Hol: "+parts[0]);
+			return [dátum, parts[0]];
+		}
+	});
 }
