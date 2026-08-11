ecc4510fe85874c09c9aa468ae7cac552ce69eeb
Csa.Csi87 <csalos.csilla@gmail.com>
2026-08-11 09:03:03 +0200

    Create apiCall.js

---
+++ apiCall.js
@@ -0,0 +1,24 @@
+// megye statisztika lekérése
+export function xstat(myUserId) {
+  const response = await fetch("https://api.geocaching.hu/mstat?userid="+myUserId);
+  if (!response.ok) throw new Error("API hívás sikertelen: mStat");
+  const json = await response.json();
+  return json;
+}
+
+// mozgó statisztika lekérése
+export function xstat(myUserId) {
+  const response = await fetch("https://api.geocaching.hu/xstat?userid="+myUserId);
+  if (!response.ok) throw new Error("API hívás sikertelen: xStat");
+  const json = await response.json();
+  return json;
+}
+
+// összes log lekérése
+export function logsbyuser(myUserId) {
+  const response = await fetch("https://api.geocaching.hu/logsbyuser?fields=cache_id%2Cdate%2Cnotes%2Clogtype&dir=asc&userid="+myUserId);
+  if (!response.ok) throw new Error("API hívás sikertelen: logsByUser");
+  const json = await response.json();
+  return json;
+}
+
