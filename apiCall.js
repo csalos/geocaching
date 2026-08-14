1deb08d12c4999ead3942155bb7c3caea2de887e
Csa.Csi87 <csalos.csilla@gmail.com>
2026-08-14 12:04:38 +0200

    only getRecord()

--- apiCall.js
+++ apiCall.js
@@ -1,32 +1,11 @@
-// megye statisztika lekérése
-export async function mstat(myUserId) {
-  const response = await fetch("https://api.geocaching.hu/mstat?userid="+myUserId);
-  if (!response.ok) throw new Error("API hívás sikertelen: mStat");
-  const json = await response.json();
-  return json;
-}
-
-// mozgó statisztika lekérése
-export async function xstat(myUserId) {
-  const response = await fetch("https://api.geocaching.hu/xstat?userid="+myUserId);
-  if (!response.ok) throw new Error("API hívás sikertelen: xStat");
-  const json = await response.json();
-  return json;
-}
-
-// összes log lekérése
-export async function logsbyuser(myUserId) {
-  const response = await fetch("https://api.geocaching.hu/logsbyuser?fields=cache_id%2Cdate%2Cnotes%2Clogtype&dir=asc&userid="+myUserId);
-  if (!response.ok) throw new Error("API hívás sikertelen: logsByUser");
-  const json = await response.json();
-  return json;
-}
-
 // --- KONFIGURÁCIÓ ---
 const STORAGE_KEY = "csacsi_api_storage";    // A tároló kulcsa a sessionStorage-ben
 const TIMESTAMP_KEY = "csacsi_api_time";    // Az időbélyeg kulcsa
 const EXPIRATION_TIME_MS = 15 * 60 * 1000;  // 15 perc ezredmásodpercben (15 * 60 * 1000)
 
+// megye statisztika lekérése
+// mozgó statisztika lekérése
+// összes log lekérése
 export async function getRecord(what, myUserId) {
     const now = Date.now();
     
