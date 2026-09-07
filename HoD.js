48ad98889ebc5a22cd5bda001908f059987d7310
Csa.Csi87 <csalos.csilla@gmail.com>
2026-09-07 18:50:15 +0200

    Update HoD.js

--- HoD.js
+++ HoD.js
@@ -3,19 +3,16 @@ document.write("<button style='height: 100%; width: 100%' onclick='HoD(this)'>St
 
 // Az adott névvel ellátott DIV elemet lehet macerálni
 var divStats = document.getElementById("divStats");
-var sh= false;
 /*
 /* Felhasználói oldalon elhelyezett DIV elem elrejtése, vagy megmutatása
 * @param btn [button] - a kinyitó és becsokú gomb  
 */
 function HoD(btn) {
-	if(sh) {
-		divStats.style.display = "none";
-		btn.innerHTML = "Statisztika kinyitása";
-		sh = false;
-    } else {
+	if(divStats.style.display == "none") {
 		divStats.style.display = "";
 		btn.innerHTML = "Statisztika összecsukása";
-		sh = true;
+    } else {
+		divStats.style.display = "none";
+		btn.innerHTML = "Statisztika kinyitása";
     }
 }
