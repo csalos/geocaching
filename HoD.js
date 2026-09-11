6017f40dcf9e8a63f559d6f1a5ccfd8125197560
Csa.Csi87 <csalos.csilla@gmail.com>
2026-09-11 22:41:02 +0200

    Update HoD.js

--- HoD.js
+++ HoD.js
@@ -13,15 +13,15 @@ btn.style.height = "33px";
 btn.style.width = "100%";
 btn.id = "HoD_button";
 btn.onclick = HoD; // Gombnyomásra lefut a HoD függvény
-btn.innerHTML = "Statisztika "+ ((divStats.style.display == "none")?"kinyitása":"összecsukása");
+btn.innerHTML = (divStats.style.display == "none")?"Kinyit":"Becsuk";
 
-// Beszúrás a script elé
+// Gomb beszúrása a script elé
 if (document.currentScript) {
     document.currentScript.parentNode.insertBefore(btn, document.currentScript);
 }
 
 // Felhasználói oldalon elhelyezett DIV elem elrejtése, vagy megmutatása
 function HoD() {
-	btn.innerHTML = "Statisztika "+ ((divStats.style.display == "none")?"összecsukása":"kinyitása");
+	btn.innerHTML = (divStats.style.display == "none")?"Becsuk":"Kinyit";
 	divStats.style.display = (divStats.style.display == "none")?"":"none";
 }
