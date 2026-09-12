87a0778876ff0c29555cfb3fe5b68c526393cf08
Csa.Csi87 <csalos.csilla@gmail.com>
2026-09-12 22:08:27 +0200

    back to yesterday

--- megye.js
+++ megye.js
@@ -104,17 +104,12 @@ function svgManipulator(svgBelseje) {
 			let rad = percent * 3.6 * (Math.PI / 180);
 			let x = Math.sin(rad)*100 + 100;
 			let y = Math.cos(rad)*100 + 100;
-			
+
 			pattern = document.createElementNS("http://www.w3.org/2000/svg", "pattern");
 			pattern.id= megye + "_pattern";
 			pattern.setAttribute("viewBox", "0,0,200,200");
-			pattern.setAttribute("width", "200");
-			pattern.setAttribute("height", "200");
-			pattern.setAttribute('x', 50);
-			pattern.setAttribute('y', 0);
-			pattern.setAttribute("preserveAspectRatio", "xMidYMid");
-			pattern.setAttribute("patternUnits","objectBoundingBox");
-   			pattern.setAttribute("patternTransform", "matrix(0.01,0,0,0.01,10040.925,7989.815)");
+			pattern.setAttribute("width", "200%");
+			pattern.setAttribute("height", "200%");
 
 			let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
 			path.id = megye + "_path";
@@ -122,6 +117,7 @@ function svgManipulator(svgBelseje) {
 			path.setAttribute("style", "fill: url(#RG"+c+")");
 			path.setAttribute("stroke", "black");
 			path.setAttribute("strokeWidth", 0);
+			path.setAttribute("transform", "translate(-50 -50)");
 
 			if(percent > 99.9) { 
 				path.setAttribute("d", "M 100 200 A 100 100, 0, 0, 0, 100 0 A 100 100, 0, 0, 0, 100 200 Z");
